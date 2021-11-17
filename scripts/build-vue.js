e8dbb20892d397597ec9ffc818dae242ca3e1dad
Bart Ramaekers <bartramaekers99@gmail.com>
2021-11-18 00:22:52 +0100

    Use ElectronBuilder CLI instead of programmatic API

--- scripts/build-vue.js
+++ scripts/build-vue.js
@@ -3,14 +3,11 @@ function build() {
     const Vite = require('vite');
     const viteConfig = require(Path.join(__dirname, '..', 'config', 'vite.js'));
 
-    return Vite.build({
+    Vite.build({
         ...viteConfig,
-        /**
-         * Base set to './' on production for Electron app. Vite sets wrong bundle path otherwise.
-         */
         base: './',
         mode: 'production'
     });
 }
 
-module.exports = build; 
+build();
