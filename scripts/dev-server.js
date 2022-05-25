334d6a6f6a37df2f1810c74662c9ef3be5012e5f
Bart Ramaekers <bartramaekers99@gmail.com>
2022-05-25 11:31:12 +0200

    Prevent spawning of electron process on tsc error

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -34,7 +34,7 @@ async function startElectron() {
         await compileTs(Path.join(__dirname, '..', 'src', 'main'));
     } catch {
         console.log(Chalk.redBright('Could not start Electron because of the above typescript error(s).'));
-        electronRestartLocker = false;
+        electronProcessLocker = false;
         return;
     }
 
