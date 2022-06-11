5772a76e6269e08bde3813af4ade2ae6a79f9095
Bart Ramaekers <32544586+Deluze@users.noreply.github.com>
2022-06-11 22:34:25 +0200

    Remove verbosity in comment

--- src/renderer/electron.ts
+++ src/renderer/electron.ts
@@ -1,6 +1,6 @@
-/**
- * This file is used to typehint the exposed electron api's.
- * The purpose of this is to get static analysis in Vue files without additional plug-ins.
+/*
+   Add all your exposed Electron API's here.
+   The purpose of this is to get static analysis in Vue files without additional plug-ins.
  */
 import { IpcRenderer } from 'electron'
 
