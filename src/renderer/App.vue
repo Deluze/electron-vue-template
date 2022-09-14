208c733b4ab8ad48c0a4ce36743f8341e6da4a3a
Bart Ramaekers <bartramaekers99@gmail.com>
2022-09-15 00:52:44 +0200

    Remove obsolete electron module in renderer

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import Hello from './components/Hello.vue'
-import { ipcRenderer } from './electron'
 
-ipcRenderer.send('message', 'Hello from App.vue!');
+window.electron.ipcRenderer.send('message', 'Hello from App.vue!');
 </script>
 
 <template>
