0d3978b3b1aaf38a36a9136f27f8c71d81798688
Bart Ramaekers <bartramaekers99@gmail.com>
2023-03-02 21:55:03 +0100

    Use Vite's new Vue template

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-import Hello from './components/Hello.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 window.electronAPI.sendMessage('Hello from App.vue!');
 </script>
 
 <template>
-  <div id="app">
-    <Hello/>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
