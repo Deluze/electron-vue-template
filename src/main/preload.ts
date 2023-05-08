161e0be2fa29b089ca94c6b465f949e5eb8b00df
Bart Ramaekers <32544586+Deluze@users.noreply.github.com>
2023-05-08 13:26:15 +0200

    Formatting in preload.ts

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,5 +1,5 @@
 import {contextBridge, ipcRenderer} from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  sendMessage: (message:string) => ipcRenderer.send('message', message)
+  sendMessage: (message: string) => ipcRenderer.send('message', message)
 })
